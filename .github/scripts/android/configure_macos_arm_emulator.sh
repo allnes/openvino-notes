caad5a62eae251bbb7df6a066f3b95bd63ed53c1
Alexander Nesterov <nesterov.alexander@outlook.com>
2026-03-12 01:48:05 +0100

    ci: run arm emulators on linux x86 hosts

--- .github/scripts/android/configure_macos_arm_emulator.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-android_config_dir="${HOME}/.android"
-mkdir -p "${android_config_dir}"
-
-cat > "${android_config_dir}/advancedFeatures.ini" <<'EOF'
-HVF = off
-EOF
